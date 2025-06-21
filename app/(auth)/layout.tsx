9808159c2b561470897dfe38410127bb63e8aab9
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2025-06-21 16:42:59 +0200

    Add styles on signin and signup

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -6,8 +6,8 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-50 to-indigo-100">
-      <div className="w-full max-w-md">{children}</div>
+    <div className="min-h-screen w-full antialiased font-primary">
+      {children}
     </div>
   );
 }
